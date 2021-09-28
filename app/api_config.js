73e02fbbadff400e96289bcd8fe4766c2bb50fbc
AndySimph <andytsimphaly@gmail.com>
2021-09-28 15:57:45 -0500

    Got rid of files for changing to expo

--- app/api_config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-const config = {
-  APCA_API_KEY_ID: 'key_id',
-  APCA_API_SECRET_KEY: 'secret_key',
-  BASE_URL: 'https://paper-api.alpaca.markets/',
-};
-
-export default config;
