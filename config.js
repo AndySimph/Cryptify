1573f6c6d8529317f1a3eb4c72730361fafa8fa7
AndySimph <andytsimphaly@gmail.com>
2021-09-28 16:09:16 -0500

    Added a config file that would hold the alpaca api info

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  APCA_API_KEY_ID: 'key_id',
+  APCA_API_SECRET_KEY: 'secret_key',
+  // BASE_URL: 'https://api.alpaca.markets/',
+  BASE_URL: 'https://paper-api.alpaca.markets/'
+}
+
+export default config;
