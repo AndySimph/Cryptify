c6ed10a100ed40111a7fab4e37322624ff21aaf2
AndySimph <andytsimphaly@gmail.com>
2021-10-20 15:32:07 -0500

    Got graphs to display, just need to get it to display much clearer

--- screens/CoinScreen.tsx
+++ screens/CoinScreen.tsx
@@ -3,15 +3,26 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import ExploreScreen from './ExploreScreen';
+import Chart from '../components/data_chart'
 
 export default function CoinScreen({ route, navigation }: RootTabScreenProps<'Coin'>) {
     const item_id = route.params;
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}></Text>
-            <Text>{item_id}</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        </View >
+        // <View style={styles.container}>
+        //     <Text style={styles.title}></Text>
+        //     <Text>{item_id.name}</Text>
+        //     <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        // </View >
+        
+        <Chart
+            currentPrice={item_id.currentPrice}
+            logoUrl={item_id.image}
+            name={item_id.name}
+            symbol={item_id.symbol}
+            priceChangePercentage7d={item_id.priceChangePercentage7d}
+            sparkline={item_id.sparkline}
+        />
+            
     );
 }
 
@@ -19,7 +30,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        //justifyContent: 'center',
     },
     title: {
         fontSize: 20,
