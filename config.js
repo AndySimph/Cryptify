cfcd8b940d39cc3f98afda738cdfea8bed3f053e
unknown <Manish>
2021-11-09 16:49:41 -0600

    added create order function to alpaca.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const config = {
   APCA_API_SECRET_KEY: 'secret_key',
   // BASE_URL: 'https://api.alpaca.markets/',
   BASE_URL: 'https://paper-api.alpaca.markets/'
+  ORDERS_URL = 'https://paper-api.alpaca.markets/v2/orders'
 }
 
 export default config;
