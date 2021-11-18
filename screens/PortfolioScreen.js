8a351e5288a543a61ab21b019720be44ad290b75
Nick Beffa <slick85609@gmail.com>
2021-11-17 19:11:01 -0600

    attempted to read from database, could not figure it out

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -9,6 +9,24 @@ import { IconButton, Button } from '../components'
 import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider'
 
 const auth = Firebase.auth();
+db = Firebase.firestore()
+
+//class that would store data from database document (only balance for now)
+class userData {
+    constructor(balance) {
+        this.balance = balance;
+    }
+    getBalance() {
+        return this.balance;
+    }
+}
+//function used to pull data from document and return new object with data
+var balanceGet = {
+    fromFirestore: function(snapshot, options){
+        const data = snapshot.data(options)
+        return new userData(data.balance);
+    }
+}
 
 export default function PortfolioScreen() {
     const { user } = useContext(AuthenticatedUserContext);
@@ -20,15 +38,23 @@ export default function PortfolioScreen() {
         }
     };
     const uid = user.uid;
-    const incBalance = async() => {
-        Firebase.database().ref('users/').push({
-            balance: 1000
-        });
-    }
+    let balance = 0;
+    //get document from database
+    db.collection("users").doc(uid)
+        .withConverter(balanceGet)
+        .get().then((doc) => {
+            //store data in new object
+            var uData = doc.data();
+            //put balance into a variable to use
+            balance = uData.getBalance();
+            //console.log(uData.getBalance());
+        })
+    //text with {balance} would display balance of the user
     return (
         <View style={styles.container}>
             <Text style={styles.title2}>{user.email}</Text>
             <Text style={styles.title}>Portfolio</Text>
+            <Text style={styles.title}>{balance}</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
             <IconButton
                 onPress={handleSignOut}
@@ -36,16 +62,7 @@ export default function PortfolioScreen() {
                 size={24}
                 color='#000'
             />
-            <Button
-                onPress={incBalance}
-                backgroundColor='#bcbcbc'
-                title='Increase'
-                titleColor='#000'
-                titleSize={20}
-                containerStyle={{
-                    marginBottom: 24
-                }}
-            />
+            
         </View>
     )
 }
@@ -69,3 +86,14 @@ const styles = StyleSheet.create({
         width: '80%',
     },
 })
+//button that was going to be for testing
+    /* <Button
+                
+                backgroundColor='#bcbcbc'
+                title='Increase'
+                titleColor='#000'
+                titleSize={20}
+                containerStyle={{
+                    marginBottom: 24
+                }}
+            /> */
